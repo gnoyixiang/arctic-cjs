1f8a07a9b70039906565542fbc4c23768014187e
gnoyixiang <gnoyixiang@gmail.com>
2025-04-28 12:22:45 +0800

    chore: update build

--- rollup.config.cjs
+++ rollup.config.cjs
@@ -1,5 +1,5 @@
-import typescript from "@rollup/plugin-typescript";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import { cleandir as cleanDir } from "rollup-plugin-cleandir";
 
@@ -17,17 +17,13 @@ export default [
 		plugins: [
 			cleanDir(OUT_DIR),
 			typescript({
-				tsconfig: "./tsconfig.json",
+				clean: true,
 				module: "ESNext"
 			}),
-			nodeResolve({
-				preferBuiltins: true,
+			resolve({
 				extensions: [".js", ".ts"]
 			}),
-			commonjs({
-				transformMixedEsModules: true,
-				include: /node_modules/
-			})
+			commonjs()
 		]
 	}
 ];
