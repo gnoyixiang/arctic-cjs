c89a4e76c739d4c44545bf789faa3dc842388829
gnoyixiang <gnoyixiang@gmail.com>
2025-04-28 12:22:45 +0800

    chore: update build

--- src/oidc.ts
+++ src/oidc.ts
@@ -4,6 +4,7 @@ export function decodeIdToken(idToken: string): object {
 	try {
 		return jwt.decodeJWT(idToken);
 	} catch (e) {
+		// @ts-expect-error coming from original library
 		throw new Error("Invalid ID token", {
 			cause: e
 		});
